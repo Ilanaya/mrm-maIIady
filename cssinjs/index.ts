2d5a6b47023219ac50318d1542148bde79219951
Ilya Golovin <me@ilscc.ru>
2022-11-28 01:19:16 +0300

    fix(cssinjs): ensure path exists

--- cssinjs/index.ts
+++ cssinjs/index.ts
@@ -1,18 +1,17 @@
-import dedent from 'dedent'
 import { readFileSync } from 'fs'
 import { install, updateFile } from 'mrm-core'
+import { ensureFileSync } from 'fs-extra'
 
-module.exports = () => {
+module.exports = ({ preset = 'default' }) => {
     require('../tailwind/index')(false)
     // linaria@next
     install('styled-components @types/styled-components react-is twin.macro polished'.split(' '), {
         pnpm: true,
     })
-    const entrypointPath = 'src/index.tsx'
+    const entrypointPath = preset === 'react' ? './src/index.tsx' : './src/index.ts'
+    ensureFileSync(entrypointPath)
+
     const entrypointContents = readFileSync(entrypointPath, 'utf-8')
-    if (!entrypointContents.startsWith('/// <reference')) {
-        updateFile(entrypointPath, `/// <reference types="@zardoy/vit/twin-sc" />\n${entrypointContents}`, true)
-    }
     if (!entrypointContents.includes("import 'tailwindcss/base.css'")) console.error("don't forget to add tailwind base styles!")
     if (entrypointContents.includes("import 'tailwindcss/tailwind.css'")) console.error("don't forget to remove tailwind styles!")
 }
