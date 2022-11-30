7b9ec77139e7ffc49af2136d040d8f07472ec0b0
Ilya Golovin <me@ilscc.ru>
2022-11-30 03:14:05 +0300

    chore: update lock, remove vscframework

--- vsc/vscode-framework.config.js
+++
@@ -1,7 +0,0 @@
-//@ts-check
-const { defineConfig } = require('@zardoy/vscode-utils/build/defineConfig.cjs')
-const { patchPackageJson } = require('@zardoy/vscode-utils/build/patchPackageJson.cjs')
-
-patchPackageJson({})
-
-module.exports = defineConfig({})
