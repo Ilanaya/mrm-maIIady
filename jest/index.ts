bbb96d8d2fcc3fa47a5f37c4779354cb87c04942
Ilya Golovin <me@ilscc.ru>
2022-12-20 01:39:59 +0300

    feat: minor fixes, test publish

--- jest/index.ts
+++ jest/index.ts
@@ -2,13 +2,14 @@ import * as fsExtra from 'fs-extra'
 import { install, lines, packageJson } from 'mrm-core'
 import { join } from 'path'
 
+const jestConfigFile = 'jest.config.js'
+
 module.exports = () => {
     install(['jest', '@types/jest', 'esbuild-runner', 'esbuild'], {
         dev: true,
         pnpm: true,
     })
 
-    const jestConfigFile = 'jest.config.js'
     lines(jestConfigFile)
         .set([fsExtra.readFileSync(join(__dirname, jestConfigFile), 'utf-8')])
         .save()
